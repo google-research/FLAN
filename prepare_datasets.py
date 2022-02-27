15f6fa03551bca74d06464bcc838e8821fd6a0a1
USVSN SAI PRASHANTH <usaiprashanth2018@gmail.com>
2022-02-27 19:50:40 +0000

    Prepare json dataset

--- prepare_datasets.py
+++ prepare_datasets.py
@@ -1,26 +1,54 @@
 from flan import task_splits
 from flan import tasks
+import tensorflow as tf
+from tqdm import tqdm
 import seqio
 import sys
+from multiprocessing import Pool
+import json
+
+def process_prompt(prompt,response):
+    '''Process a FLAN prompt to remove extra tokens'''
+    prompt,response = list(map(lambda x:x.numpy().decode(),[prompt,response]))
+    prompt = prompt.strip('0').strip('X 1 FLAN').strip()
+    response = response.strip()
+    return f'{prompt}\n{response}'
+
+
+def process_single_example(example):
+    '''Converts a single prompt to A Decoder only format'''
+
+    inputs_pretokenized = example['inputs_pretokenized']
+    targets_pretokenized = example['targets_pretokenized']
+
+    return tf.py_function(
+        process_prompt,
+        inp=[inputs_pretokenized,targets_pretokenized],
+        Tout=tf.string
+    )
+
+def prepare_task(task):
+    '''Saves a single task data'''
+    dataset = seqio.get_mixture_or_task(task).get_dataset(
+        sequence_length={'inputs':4096,'targets':4096} # Extranous length to capture all data
+    )
+    dataset = dataset.map(process_single_example)
+    
+    data = []
+    for i in dataset.as_numpy_iterator():
+        data.append(i.decode())
+    
+    with open(f'./FLANdata/{task}.json','w') as f:
+        json.dump(data,f)
+
 
 if __name__ == '__main__':
-
-    # sys.stdout = open('out.txt','w')
-    sys.stderr = open('err.txt','w')
     splits = task_splits.generate_superglue_num_tasks_ablation()
+    task_split = splits[-1]
 
-    for task_split in splits:
-        for train_task in task_split.train_tasks:
-            dataset = seqio.get_mixture_or_task(train_task).get_dataset(
-                sequence_length={'inputs':4096,'targets':4096}
-            )
-            curr = 5
-            for ex in dataset.as_numpy_iterator():
-                
-                inputs_pretokenized = ex['inputs_pretokenized'].decode('ascii')
-                targets_pretokenized = ex['targets_pretokenized'].decode('ascii')
-                print(targets_pretokenized)
-                break
-            break
-        break
+    all_tasks = list(task_split.train_tasks)
+    all_tasks += task_split.test_tasks
+
+    with Pool(36) as p:
+        p.map(prepare_task,all_tasks)
 
